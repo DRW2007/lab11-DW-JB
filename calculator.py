382f63d187a00d9a390b1591ec09f0bb60e87b0a
drww <drw2007@icloud.com>
2025-11-12 14:06:41 -0500

    modified calculator p1

--- calculator.py
+++ calculator.py
@@ -1,11 +1,33 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/DRW2007/lab11-DW-JB.git
+# Partner 1: Daniel Wagner
+# Partner 2:
+import math
 
-One function per operation, in order.
-"""
-# First example
+
 def add(a, b): 
-    pass
+    return a + b
 
 
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if a == 0:
+        raise ZeroDivisionError("Can't divide by zero!")
+    return b / a
+
+
+def log(a, b):
+    try:
+        return math.log(b, a)
+    except ValueError:
+        raise
+
+
+def exp(a, b):
+    pow(a, b)
